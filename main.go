6e90e06328bdfbef0d9b35dc17aed3c0f6477648
dhaval dhanani <dhaval070@gmail.com>
2024-10-16 21:53:13 +0530

    fixed end time format

--- main.go
+++ main.go
@@ -92,8 +92,8 @@ func downloadReport(c *gin.Context) {
 					l.name location_name,
 					s.name surface_name,
 					date_format(e.datetime, "%W") dow,
-					date_format(min(e.datetime), "%Y-%m-%d %H:%I:%S") start_time,
-					date_format(max( date_add(e.datetime, INTERVAL 90 minute)), "%Y-%m-%d %H:%I:%S") end_time
+					date_format(min(e.datetime), "%Y-%m-%d %T") start_time,
+					date_format(max( date_add(e.datetime, INTERVAL 90 minute)), "%Y-%m-%d %T") end_time
 				FROM
 				events e JOIN surfaces s on e.surface_id=s.id JOIN locations l on l.id=s.location_id
 				GROUP BY location_name, surface_name, surface_id, date(e.datetime)
