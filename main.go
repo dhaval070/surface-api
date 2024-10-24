0409fc67003f139e651436ca5b0f8fff00c2a257
dhaval dhanani <dhaval070@gmail.com>
2024-10-24 23:25:20 +0530

    fixed group by columns in report

--- main.go
+++ main.go
@@ -101,7 +101,7 @@ func downloadReport(c *gin.Context) {
 					date_format(max( date_add(e.datetime, INTERVAL 90 minute)), "%Y-%m-%d %T") end_time
 				FROM
 				events e JOIN surfaces s on e.surface_id=s.id JOIN locations l on l.id=s.location_id
-				GROUP BY location_name, surface_name, surface_id, date(e.datetime)
+				GROUP BY l.name, s.name, e.surface_id,  e.datetime
 				ORDER BY location_name, surface_name, surface_id,dayofweek(e.datetime) `
 
 	dbh, err := db.DB()
